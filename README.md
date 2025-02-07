de7a73a4c5f2a0d48ad1f62d914005d4447ed6dc
taggu1 <hm834806@protonmail.com>
2025-02-07 18:41:30 +0200

    start

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Dust
-Creating a toy-programming-langauge using c's marcro definitions
+# Dust Programming langauge
+The one and only programming langauge, for the chosen ones...
+
+
+## Features
+- 98% less safer that rust
+- Simple syntax  
+
+## Work in progress..
+
+### WARNING ⚠️: Might ruin all other programming langaues for you
---
+++ main.c
@@ -0,0 +1,20 @@
+
+/// Yes, This is bad code... I know, no need to tell me brother
+
+#include <stdio.h>
+
+#define wholeNumber int
+#define letsGetIntoIt main
+#define Nothing void
+#define BEGIN {
+#define END }
+#define Iwant (
+#define ENOUGH )
+#define say printf
+#define END_LINE ;
+#define Give return
+
+wholeNumber letsGetIntoIt Iwant Nothing ENOUGH BEGIN
+say Iwant "Hello,world." ENOUGH END_LINE
+Give 0 END_LINE
+END
